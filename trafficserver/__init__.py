c64b09bb66c99854bd0034d4bfb59f813e7f89db
Taegyun Kim <tagun1202@gmail.com>
2020-11-11 15:33:17 +0900

    fix: random sample is now made 3 times in for loop

---
+++ trafficserver/__init__.py
@@ -0,0 +1,1 @@
+# traffic server init
--- trafficserver/traffic_server.py
+++ trafficserver/traffic_server.py
@@ -1,3 +1,4 @@
+import json
 from pprint import pprint
 from traffic import outer, inner
 import util
@@ -6,7 +7,9 @@ import util
 Input:
     [user_floor, elev_cur_floor, elev_info(calls,direction)]
 """
-if __name__ == "__main__":
+
+
+def main():
     user_floor = 3
     elev_floor = 8
     total_floors = 15
@@ -16,15 +19,17 @@ if __name__ == "__main__":
     outer_traffic = outer.Outer(total_floors)
     inner_traffic = inner.Inner(total_floors)
 
-    # create dummy data for test
-    outer_dummpy = util.generate_random_user_outer(total_floors)
     for _ in range(3):
+        outer_dummy = util.generate_random_user_outer(total_floors)
         outer_traffic.update_table(outer_dummpy)  # per day
 
-    # INNER TEST
-
-    util.generate_random_user_inner()
-    ###
+    usage_info = json.dumps({
+        "enter_nums": 4,
+        "exit_nums": 0
+    })
+    calls = [1, 3, 5, 6]
+    inner_dummy = util.generate_random_user_inner()
+    inner_traffic.update_table(floor, usage_info, calls)
 
     # predict floors
     outer_traffic_predict = outer_traffic.get_prediction(
@@ -32,7 +37,6 @@ if __name__ == "__main__":
     inner_traffic_predict = inner_traffic.get_prediction(
         user_floor, elev_floor)
 
-    # traffic_dict = dict()
-    # for floor, outer_time in outer_traffic_dict.items():
-    #     inner_time = inner_traffic_dict[floor]
-    #     traffic_dict[floor] = inner_time + outer_time
+
+if __name__ == "__main__":
+    main()
