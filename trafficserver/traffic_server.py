6ac38776e1871312b185a49ececcd9739ed7288a
Taegyun Kim <tagun1202@gmail.com>
2020-11-14 17:19:37 +0900

    fix: traffic_server and inner.py

--- trafficserver/traffic_server.py
+++ trafficserver/traffic_server.py
@@ -9,33 +9,40 @@ Input:
 """
 
 
-def main():
-    user_floor = 3
-    elev_floor = 8
-    total_floors = 15
-    calls = [2, 5, 7]
+def update_traffic():
+    pass
 
+
+def main(user_floor, elev_floor, total_floors, calls, time):
     outer_traffic = outer.Outer(total_floors)
     inner_traffic = inner.Inner(total_floors)
 
     for _ in range(3):
         outer_dummy = util.generate_random_user_outer(total_floors)
-        outer_traffic.update_table(outer_dummpy)  # per day
+        outer_traffic.update_table(outer_dummy)  # per day
 
     usage_info = json.dumps({
         "enter_nums": 4,
         "exit_nums": 0
     })
     calls = [1, 3, 5, 6]
-    inner_dummy = util.generate_random_user_inner()
-    inner_traffic.update_table(floor, usage_info, calls)
+    inner_dummy = util.generate_random_user_inner(total_floors)
+    inner_traffic.update_table(elev_floor, usage_info, calls)
 
     # predict floors
     outer_traffic_predict = outer_traffic.get_prediction(
-        user_floor, elev_floor)
+        elev_floor, user_floor, time)
     inner_traffic_predict = inner_traffic.get_prediction(
-        user_floor, elev_floor)
+        user_floor)
+
+    print(outer_traffic_predict)
+    print(inner_traffic_predict)
 
 
 if __name__ == "__main__":
-    main()
+    user_floor = 3
+    elev_floor = 8
+    total_floors = 15
+    calls = [2, 5, 7]
+    time = 14
+    main(user_floor, elev_floor, total_floors, calls, time)
